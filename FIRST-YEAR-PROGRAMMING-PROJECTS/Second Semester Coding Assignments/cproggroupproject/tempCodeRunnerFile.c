f96d3960e4750932d964c279317ebe8293afd52f
RafaelMiguel03 <miguel.rafaellouie2003@gmail.com>
2023-06-26 07:17:02 +0800

    updates

--- FIRST-YEAR-PROGRAMMING-PROJECTS/Second Semester Coding Assignments/cproggroupproject/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-while (scanf("%d", &size) != 1)	{
-//     	printf("INVALID INPUT. Please enter numbers only.\n");
-//     	printf("Input the number of elements to be stored in the array: ");
-//     	while (getchar() != '\n');
-// 	}
